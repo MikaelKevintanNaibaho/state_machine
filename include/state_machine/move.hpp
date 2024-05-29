0b040e20d9a3d9442b2c078c970bad26c40c5a40
MikaelKevintanNaibaho <mikaelkevintannaibaho@gmail.com>
2024-05-29 08:47:15 +0700

    fix

--- include/state_machine/move.hpp
+++ include/state_machine/move.hpp
@@ -58,7 +58,7 @@ void generate_walk_back_leg(struct bezier2d *curve, SpiderLeg *leg, float stride
 void generate_stright_back_trajectory(struct bezier2d *stright_back, SpiderLeg *leg,
                                       float stride_length);
 
-void generate_circular_trajectory(struct bezier3d *curve, SpiderLeg *leg, float radius, 
+void generate_circular_trajectory(struct bezier2d *curve, SpiderLeg *leg, float radius, 
                                   float swing_height, float angle_offset) ;
 
 // printing and saving//
